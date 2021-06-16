f2b2a3b66b437b7c769cff190968e6da26bdd032
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-16 12:13:43 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let question1 = $("#question1")
+let question2 = $("#question2")
+let question3 = $("#question3")
+let question4 = $("#question4")
+let question5 = $("#question5")
+let question6 = $("#question6")
+let question7 = $("#question7")
+let question8 = $("#question8")
+let question9 = $("#question9")
+let question10 = $("#question10")
+// let question1 = $("#question1")
+// let question1 = $("#question1")
+// let question1 = $("#question1")
+function validateForm(){
+    question1.each()
+    //.isChecked()
+}
